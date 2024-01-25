99c33f1eea1635de72da8c5bcebd32e7d730d27f
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-01-25 11:04:29 +0530

    setup axios public and axios private to send requests

--- src/services/users.js
+++ src/services/users.js
@@ -1,28 +1,29 @@
-import axios from "axios";
+import { axiosPublic } from "../common/axiosPublic";
+import { axiosPrivate } from "../common/axiosPrivate"
 
 const backendURL = import.meta.env.VITE_BACKEND_URL
 
 export const loginUser = async (data) => {
-  const response = await axios.post(`${backendURL}/users/login`, data)
+  const response = await axiosPublic.post(`${backendURL}/users/login`, data, { withCredentials: true })
   return response
 }
 
 export const registerUser = async (data) => {
-  const response = await axios.post(`${backendURL}/users/register`, data)
+  const response = await axiosPublic.post(`${backendURL}/users/register`, data)
   return response
 }
 
 export const logoutUser = async (data) => {
-  const response = await axios.delete(`${backendURL}/users/logout`)
+  const response = await axiosPrivate.delete(`${backendURL}/users/logout`)
   return response
 }
 
 export const forgetPassword = async (data) => {
-  const response = await axios.post(`${backendURL}/users/forget-password`, data)
+  const response = await axiosPublic.post(`${backendURL}/users/forget-password`, data)
   return response
 }
 
 export const resetPassword = async (data) => {
-  const response = await axios.post(`${backendURL}/users/reset-password`, data)
+  const response = await axiosPublic.post(`${backendURL}/users/reset-password`, data)
   return response
 }
