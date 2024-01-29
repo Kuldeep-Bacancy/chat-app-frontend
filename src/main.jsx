2ebfee42e42270103d9a6d605aaa2a9625f97588
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-01-29 14:11:37 +0530

    Added react query and configure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import './index.css'
 import Layout from "./components/others/Layout"
 import RouteError from "./components/others/RouteError"
@@ -15,6 +16,7 @@ import { Provider } from 'react-redux'
 import AuthLayout from './components/AuthLayout'
 import PublicLayout from './components/PublicLayout'
 
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -74,7 +76,9 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 )
