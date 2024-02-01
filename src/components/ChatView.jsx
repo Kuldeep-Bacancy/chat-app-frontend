9edd0dbcc3dda81edec8e9581b0ea70bb91b4eb7
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-01 10:14:42 +0530

    Added loader while loading messages

--- src/components/ChatView.jsx
+++ src/components/ChatView.jsx
@@ -8,10 +8,10 @@ import { useForm } from 'react-hook-form';
 import io from "socket.io-client"
 
 function ChatView({ chatId }) {
-  const socket = useMemo(() => { 
-    return io(import.meta.env.VITE_SOCKET_URL, { transports: ['websocket', 'polling', 'flashsocket'] }) 
-  },[chatId])
-  
+  const socket = useMemo(() => {
+    return io(import.meta.env.VITE_SOCKET_URL, { transports: ['websocket', 'polling', 'flashsocket'] })
+  }, [chatId])
+
 
   const queryClient = useQueryClient()
   const userInfo = useSelector((state) => state.authentication.userData);
@@ -21,7 +21,7 @@ function ChatView({ chatId }) {
   const { register, handleSubmit, reset } = useForm();
 
   useEffect(() => {
-    if(chatId) {
+    if (chatId) {
       socket.emit("join-chat", chatId)
       socket.on("typing", () => setIsTyping(true));
       socket.on("stop typing", () => setIsTyping(false));
@@ -107,68 +107,90 @@ function ChatView({ chatId }) {
 
   return (
     <div className="flex flex-col bg-gray-400 w-2/3 p-2">
-      <div className="flex items-center justify-between mb-2 bg-blue-500 p-2 rounded-md">
-        <h2 className="text-xl font-bold text-white">{chatName}</h2>
-        {chatData?.isGroupChat && (
-          <button className="text-white hover:text-gray-200" onClick={() => setShowModal(true)}>
-            <svg xmlns="http://www.w3.org/2000/s/*-/*//-/-/-/-/-/-*//*/-*/-*/-*/vg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 14 12 8" />
-              <circle cx="12" cy="12" r="10" />
-            </svg>
-          </button>
-        )}
-        {showModal && <GroupSettingsModal setShowModal={setShowModal} groupId={chatData._id} groupUsers={groupUsers} />}
-      </div>
+      {
+        chatId 
+        ?
 
-      {/* Messages with selected person */}
-      <div className="flex-grow mb-2">
-        {messages?.data?.data?.data.map((message, index) => (
-          <div key={index} className={`flex items-start ${message.sender._id === userInfo._id ? 'justify-end' : 'justify-start'}`}>
-            <div className={`bg-${message.sender._id === userInfo._id ? 'blue' : 'yellow'}-500 text-white p-2 rounded-md mb-1`}>
-              <div className="flex items-center">
-                {
-                  message.chat?.isGroupChat && <div className="font-bold mr-2">{message.sender.username}:</div>
-                }
-                <div>{message.content}</div>
+        (
+          <>
+            <div className="flex items-center justify-between mb-2 bg-blue-500 p-2 rounded-md">
+              <h2 className="text-xl font-bold text-white">{chatName}</h2>
+              {chatData?.isGroupChat && (
+                <button className="text-white hover:text-gray-200" onClick={() => setShowModal(true)}>
+                  <svg xmlns="http://www.w3.org/2000/s/*-/*//-/-/-/-/-/-*//*/-*/-*/-*/vg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 14 12 8" />
+                    <circle cx="12" cy="12" r="10" />
+                  </svg>
+                </button>
+              )}
+              {showModal && <GroupSettingsModal setShowModal={setShowModal} groupId={chatData._id} groupUsers={groupUsers} />}
+            </div>
+            <div className="flex-grow mb-2">
+              {
+                messages.isLoading ? (
+                    <div className="text-center p-4 bg-gray-200 rounded-lg w-full">
+                      Loading messages...
+                    </div>
+                ) :
+                (
+                  messages?.data?.data?.data.map((message, index) => (
+                    <div key={index} className={`flex items-start ${message.sender._id === userInfo._id ? 'justify-end' : 'justify-start'}`}>
+                      <div className={`bg-${message.sender._id === userInfo._id ? 'blue' : 'yellow'}-500 text-white p-2 rounded-md mb-1`}>
+                        <div className="flex items-center">
+                          {
+                            message.chat?.isGroupChat && <div className="font-bold mr-2">{message.sender.username}:</div>
+                          }
+                          <div>{message.content}</div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                )
+              }
+            </div>
+            {
+              istyping &&
+              <div className="flex items-center justify-start mb-2">
+                <div className="animate-typing">
+                  <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+                  <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
+                  <div className="w-4 h-4 bg-blue-500 rounded-full"></div>
+                </div>
               </div>
+            }
+            <div className="flex gap-2 mx-2">
+              <form onSubmit={handleSubmit(sendMessageSubmitHandler)} className="flex-grow flex items-center">
+                <input
+                  type="text"
+                  placeholder="Type your message here"
+                  className="bg-white border p-2 rounded-l-md focus:outline-none focus:border-blue-500 flex-grow"
+                  {...register('content', { required: 'Content is required!' })}
+                  onChange={typingHandler}
+                />
+                <button
+                  className="bg-blue-500 p-2 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                  type="submit"
+                >
+                  {/* Change the send icon or use your own */}
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                  </svg>
+                </button>
+              </form>
+            </div>
+          </>
+        )
+
+        :
+
+        (
+          <div className="flex flex-col h-full">
+            <div className="text-center p-4 bg-yellow-200 rounded-lg w-full">
+              Click on a chat to start a conversation
             </div>
           </div>
-        ))}
-      </div>
-
-      {
-        istyping &&
-
-        <div className="flex items-center justify-start mb-2">
-          <div className="animate-typing">
-            <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
-            <div className="w-4 h-4 bg-blue-500 rounded-full mr-2"></div>
-            <div className="w-4 h-4 bg-blue-500 rounded-full"></div>
-          </div>
-        </div>
+        )
       }
-
-      {/* Input for new messages */}
-      <div className="flex gap-2 mx-2">
-        <form onSubmit={handleSubmit(sendMessageSubmitHandler)} className="flex-grow flex items-center">
-          <input
-            type="text"
-            placeholder="Type your message here"
-            className="bg-white border p-2 rounded-l-md focus:outline-none focus:border-blue-500 flex-grow"
-            {...register('content', { required: 'Content is required!' })}
-            onChange={typingHandler}
-          />
-          <button
-            className="bg-blue-500 p-2 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-            type="submit"
-          >
-            {/* Change the send icon or use your own */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-            </svg>
-          </button>
-        </form>
-      </div>
     </div>
   );
 }
