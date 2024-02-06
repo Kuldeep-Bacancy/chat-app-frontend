71c8417757d551a7b2609e7fa1520a335fe5d460
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-06 10:05:46 +0530

    Added individual delete message button for message

--- src/components/ChatView.jsx
+++ src/components/ChatView.jsx
@@ -23,6 +23,7 @@ function ChatView({ chatId }) {
   const [typing, setTyping] = useState(false);
   const [istyping, setIsTyping] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
+  const [hoveredMessage, setHoverdMessage] = useState(null)
   const { register, handleSubmit, reset } = useForm();
   const optionsRef = useRef(null);
 
@@ -213,8 +214,13 @@ function ChatView({ chatId }) {
                   ) :
                     (
                       messages?.data?.data?.data.map((message, index) => (
-                        <div key={index} className={`flex items-start ${message.sender._id === userInfo._id ? 'justify-end' : 'justify-start'}`}>
-                          <div className={`bg-${message.sender._id === userInfo._id ? 'blue' : 'yellow'}-500 text-white p-2 rounded-md mb-1`}>
+                        <div key={index} className={`flex items-start ${message.sender._id === userInfo._id ? 'justify-end' : 'justify-start'}`}
+                        >
+                          <div
+                            className={`bg-${message.sender._id === userInfo._id ? 'blue' : 'yellow'}-500 text-white p-2 rounded-md mb-1 relative`}
+                            onMouseEnter={() => setHoverdMessage(message._id)}
+                            onMouseLeave={() => setHoverdMessage(null)}
+                          >
                             <div className="flex items-center">
                               {message.chat?.isGroupChat && <div className="font-bold mr-2">{message.sender.username}:</div>}
                               <div>{message.content}</div>
@@ -231,6 +237,18 @@ function ChatView({ chatId }) {
                               </div>
                             )}
                             <div className="text-xs text-gray-500 mt-1">{formatTimestamp(message.createdAt)}</div>
+                            { /* Delete Button */}
+                            {
+                              hoveredMessage === message._id && (
+                                <button
+                                  className="absolute top-0 right-0 text-white hover:text-gray-200 focus:outline-none ml-2"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                  </svg>
+                                </button>
+                              )
+                            }
                           </div>
                         </div>
                       ))
