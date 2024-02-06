ddaa8fb9c85091fc9c5dabac28ad56b90fb663e7
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-06 14:58:16 +0530

    Added loading list for contacts

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { getCurrentUserChats } from '../services/chats';
 import { useSelector } from 'react-redux';
 import ChatView from './ChatView';
+import LoadingList from './others/LoadingList';
 
 function Chat() {
   const { data: chatsData, isLoading, isError } = useQuery({
@@ -19,25 +20,25 @@ function Chat() {
       <div className="bg-gray-200 w-1/3 border-r border-gray-300">
         <div className="p-4">
           <h2 className="text-xl font-semibold mb-4">Contacts</h2>
-          {isLoading && <p>Loading...</p>}
+          {isLoading && <LoadingList count={4}/>}
           {isError && <p>Error loading chats</p>}
           {chatsData && (
             <ul>
               {
                 chatsData.data.data.map((chat) => (
-                <li
-                  key={chat._id}
-                  className={`mb-1 cursor-pointer border-b border-gray-300 py-2 px-4 ${selectedChat === chat._id ? 'bg-blue-200' : 'hover:bg-blue-100'
-                    }`}
-                  onClick={() => setSelectedChat(chat._id)}
-                >
-                  {chat.isGroupChat ? (
-                    <span className="text-blue-500">{chat.name}</span>
-                  ) : (
-                    chat.users.find((user) => user._id !== userInfo._id)?.username
-                  )}
-                </li>
-              ))
+                  <li
+                    key={chat._id}
+                    className={`mb-1 cursor-pointer border-b border-gray-300 py-2 px-4 ${selectedChat === chat._id ? 'bg-blue-200' : 'hover:bg-blue-100'
+                      }`}
+                    onClick={() => setSelectedChat(chat._id)}
+                  >
+                    {chat.isGroupChat ? (
+                      <span className="text-blue-500">{chat.name}</span>
+                    ) : (
+                      chat.users.find((user) => user._id !== userInfo._id)?.username
+                    )}
+                  </li>
+                ))
               }
             </ul>
           )}
---
+++ src/components/others/LoadingList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function LoadingList({count = 2}) {
+  return (
+    <ul>
+      {
+        Array.from({ length: count }, (_, index) => (
+          <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full m-4" key={index}>
+            <div class="animate-pulse flex space-x-4">
+              <div class="rounded-full bg-slate-700 h-10 w-10"></div>
+              <div class="flex-1 space-y-6 py-1">
+                <div class="h-2 bg-slate-700 rounded"></div>
+                <div class="space-y-3">
+                  <div class="grid grid-cols-3 gap-4">
+                    <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+                    <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+                  </div>
+                  <div class="h-2 bg-slate-700 rounded"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+      ))
+      }
+    </ul>
+  )
+}
+
+export default LoadingList
